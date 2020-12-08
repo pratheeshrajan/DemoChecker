2f16a654917141820dbf663d5cc8d3900822582d
Pratheesh Rajan <pratheeshrajan18@gmail.com>
2020-12-08 10:52:49 +0530

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
 
         render() {
             return ( 
-                <footer className="bdT text-center p-30 lh-0 fsz-sm c-grey-600"><span>Copyright © 2017 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span>
+                <footer className="bdT text-center p-30 lh-0 fsz-sm c-grey-600"> <span>All rights reserved.</span>
 
                 </footer>
                 );
